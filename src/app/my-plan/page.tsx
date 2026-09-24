9351d13fb9df7e7cf704dca5466a07cc18da19b3
Muhammad Ziaur Rahman <ziaur.contact@gmail.com>
2026-09-24 18:10:42 +0600

    Navbar Added

---
+++ src/app/my-plan/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyPlanPage = () => {
+  return (
+    <div className="bg-global-background text-global-text">
+      My Plan Page Dummy
+    </div>
+  );
+};
+
+export default MyPlanPage;
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import React from "react";
+import logo from "@/assets/logo.png";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <div className="bg-global-background py-6 border-b-2 border-navbar-bottom">
+      <nav className="container mx-auto flex justify-between items-center">
+        {/* Navbar Left Logo */}
+        <Link href={process.env.NEXT_PUBLIC_SERVER_BASE_URL!}>
+          <div className="flex gap-3 items-center cursor-pointer">
+            <Image src={logo} alt="Navbar logo"></Image>
+            <div className="text-2xl font-semibold text-global-text">
+              FITLOG
+            </div>
+          </div>
+        </Link>
+        {/* Navbar Central Links */}
+        <div>
+          <ul className="flex justify-between items-center gap-4">
+            <li>
+              <Link
+                href={process.env.NEXT_PUBLIC_SERVER_BASE_URL!}
+                className="text-global-text"
+              >
+                Workouts
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL!}/my-plan`}
+                className="text-global-text"
+              >
+                My Plan
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {/* Navbar Right Buttons */}
+        <div className="flex justify-between items-center gap-4">
+          <button className="rounded-full border-0">
+            <Link
+              href={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL!}/my-plan`}
+              className="text-global-text"
+            >
+              Plan
+            </Link>
+          </button>
+          <button className="rounded-full border-0">
+            <Link
+              href={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL!}/my-plan`}
+              className="text-global-text"
+            >
+              Saved
+            </Link>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
